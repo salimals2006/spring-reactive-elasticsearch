c3167df534699c10ae673a1d1cac581065e2fb9e
salimals2006 <alsaba.boston@gmail.com>
2023-06-27 09:36:18 -0400

    First commit

---
+++ src/main/java/com/mac/springreactiveelasticsearch/SpringReactiveElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.mac.springreactiveelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mac/springreactiveelasticsearch/SpringReactiveElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.springreactiveelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
